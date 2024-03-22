1585f4da6fc6d2b8ad22448b9fb84843a957a519
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-21 21:18:48 -0600

    Add files via upload

--- a.c
+++ a.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 
-typedef struct {
-    int numerador;
-    int denominador;
-} Fraccion;
-
-Fraccion sumarFracciones(Fraccion fraccion1, Fraccion fraccion2) {
-    Fraccion resultado;
-    resultado.numerador = (fraccion1.numerador * fraccion2.denominador) + (fraccion2.numerador * fraccion1.denominador);
-    resultado.denominador = fraccion1.denominador * fraccion2.denominador;
-    return resultado;
-}
-
 int main() {
-    Fraccion fraccion1, fraccion2, resultado;
-
-    // Fracción 1
-    printf("Ingrese la primera fracción en el formato 'numerador/denominador': ");
-    scanf("%d/%d", &fraccion1.numerador, &fraccion1.denominador);
-
-    // Fracción 2
-    printf("Ingrese la segunda fracción en el formato 'numerador/denominador': ");
-    scanf("%d/%d", &fraccion2.numerador, &fraccion2.denominador);
-
-    // Suma de fracciones
-    resultado = sumarFracciones(fraccion1, fraccion2);
-
-    // Mostrar resultado
-    printf("La suma de las fracciones es: %d/%d\n", resultado.numerador, resultado.denominador);
-
+    int n, i = 1, suma = 0;
+    
+    printf("Ingrese un número entero positivo: ");
+    scanf("%d", &n);
+    
+    while (i <= n) {
+        suma += i;
+        i++;
+    }
+    
+    printf("La suma de los primeros %d números naturales es: %d\n", n, suma);
+    
     return 0;
 }
